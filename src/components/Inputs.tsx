f37c92efe6fe7d753c9505a302acdb0fa1b8c0cc
Nolhan#2508 <nolhan.dev@gmail.com>
2023-02-16 13:45:48 +0100

    Update Inputs.tsx

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -15,16 +15,16 @@ function Button (props: ButtonProps) {
     return (
         <>
             {
-                props.type === "primary" ? <span style={{ cursor: "pointer" }}>{ props.redirect ? <Link className={ButtonStyle.link} href={props.redirect} target={props.redirect.startsWith("http") ? "_blank" : "_parent"}><button onClick={props.onClick} className={ButtonStyle.primary}  >{props.label}</button></Link> : <button onClick={props.onClick} className={ButtonStyle.primary}  >{props.label}</button> }</span> : null
+                props.type === "primary" ? <span style={{ cursor: "pointer" }}>{ props.redirect ? <Link className={ButtonStyle.link} href={props.redirect}><button onClick={props.onClick} className={ButtonStyle.primary}  >{props.label}</button></Link> : <button onClick={props.onClick} className={ButtonStyle.primary}  >{props.label}</button> }</span> : null
             }
             {
-                props.type === "secondary" ? <span style={{ cursor: "pointer" }}>{ props.redirect ? <Link className={ButtonStyle.link} href={props.redirect} target={props.redirect.startsWith("http") ? "_blank" : "_parent"}><button onClick={props.onClick} className={ButtonStyle.secondary}  >{props.label}</button></Link> : <button onClick={props.onClick} className={ButtonStyle.secondary}  >{props.label}</button> }</span> : null
+                props.type === "secondary" ? <span style={{ cursor: "pointer" }}>{ props.redirect ? <Link className={ButtonStyle.link} href={props.redirect}><button onClick={props.onClick} className={ButtonStyle.secondary}  >{props.label}</button></Link> : <button onClick={props.onClick} className={ButtonStyle.secondary}  >{props.label}</button> }</span> : null
             }
             {
-                props.type === "tertiary" ? <span style={{ cursor: "pointer" }}>{ props.redirect ? <Link className={ButtonStyle.link} href={props.redirect} target={props.redirect.startsWith("http") ? "_blank" : "_parent"}><button onClick={props.onClick} className={ButtonStyle.tertiary}  >{props.label}</button></Link> : <button onClick={props.onClick} className={ButtonStyle.tertiary}  >{props.label}</button> }</span> : null
+                props.type === "tertiary" ? <span style={{ cursor: "pointer" }}>{ props.redirect ? <Link className={ButtonStyle.link} href={props.redirect}><button onClick={props.onClick} className={ButtonStyle.tertiary}  >{props.label}</button></Link> : <button onClick={props.onClick} className={ButtonStyle.tertiary}  >{props.label}</button> }</span> : null
             }
             {
-                props.type === "iconic" && props.icon ? <span style={{ cursor: "pointer" }}>{ props.redirect ? <Link className={ButtonStyle.link} href={props.redirect} target={props.redirect.startsWith("http") ? "_blank" : "_parent"}><button onClick={props.onClick} className={ButtonStyle.iconic}  ><img className={ButtonStyle.icon} src={props.icon} alt={props.label}/>{props.label}</button></Link> : <button onClick={props.onClick} className={ButtonStyle.iconic}  ><img className={ButtonStyle.icon} src={props.icon} alt={props.label}/>{props.label}</button> }</span> : null
+                props.type === "iconic" && props.icon ? <span style={{ cursor: "pointer" }}>{ props.redirect ? <Link className={ButtonStyle.link} href={props.redirect}><button onClick={props.onClick} className={ButtonStyle.iconic}  ><img className={ButtonStyle.icon} src={props.icon} alt={props.label}/>{props.label}</button></Link> : <button onClick={props.onClick} className={ButtonStyle.iconic}  ><img className={ButtonStyle.icon} src={props.icon} alt={props.label}/>{props.label}</button> }</span> : null
             }
         </>
     )
